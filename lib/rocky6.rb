171becddf04ced684b05cd58a6a94f6a7014032b
David Lewis and Logan Tanous <david@>
2018-02-07 09:47:43 -0800

    Add hash to return all possible combinations

--- lib/rocky6.rb
+++ lib/rocky6.rb
@@ -1,9 +1,11 @@
 class RPS
   def winner_calc?(player_input, computer_input)
     puts "Enter either 'rock', 'paper', or scissors"
-    if (player_input == "rock") & (computer_input == "scissors")
-      true
+    outcomes = {"rock_rock" => "tie", "paper_paper" => "tie", "scissors_scissors" => "tie", "rock_scissors" => true, "rock_paper" => false, "scissors_rock" => false, "scissors_paper" => true, "paper_rock" => true, "paper_scissors" => false}
 
-    end
+    input = player_input + "_" + computer_input
+
+    outcomes.fetch(input)
+
   end
 end
--- spec/rocky6_spec.rb
+++ spec/rocky6_spec.rb
@@ -18,4 +18,19 @@ describe("winner_calc") do
     expect(word1.winner_calc?("rock", "scissors")).to(eq(true))
   end
 
+  it("Compares player input and computer input and checks if it is equal") do
+    word1 = RPS.new()
+    expect(word1.winner_calc?("rock", "rock")).to(eq("tie"))
+  end
+
+  it("Compares player input and computer input and checks if player lost") do
+    word1 = RPS.new()
+    expect(word1.winner_calc?("rock", "paper")).to(eq(false))
+  end
+
+  it("Compares player input and computer input and checks if player lost") do
+    word1 = RPS.new()
+    expect(word1.winner_calc?("paper", "paper")).to(eq("tie"))
+  end
+
 end
